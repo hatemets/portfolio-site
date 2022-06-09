42c751f7880a0d5aea40a89774b6d9d82376d75a
hatemets <heidmetsmark@gmail.com>
2022-06-09 19:32:25 +0300

    Add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react"
 import logo from "./logo.svg"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 import "./styles/App.scss"
 
 const App = () => {
     return (
         <div id="App">
-            <h1>working</h1>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+            </Router>
         </div>
     )
 }
