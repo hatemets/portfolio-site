26d6b718cc015fea9b2a8dc83736c7c14d5f4d00
hatemets <heidmetsmark@gmail.com>
2022-06-09 19:21:38 +0300

    Add gatsby and sass

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./styles/App.scss"
+
+const App = () => {
+    return (
+        <div id="App">
+            <h1>working</h1>
+        </div>
+    )
+}
+
+
+export default App
