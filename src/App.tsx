b94ec65071a91c86cbf7dbe50c77dd84a902c232
hatemets <heidmetsmark@gmail.com>
2022-06-09 19:52:13 +0300

    Restructure and add navbar file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react"
 import logo from "./logo.svg"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 import "./styles/App.scss"
+
 
 const App = () => {
     return (
         <div id="App">
             <Router>
                 <Routes>
-                    <Route exact path="/" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/projects" element={<Projects />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div id="nav">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+
+export default Navbar
