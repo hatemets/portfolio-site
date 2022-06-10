9301e439a99fac38475083b273b1908d9e2db2bf
hatemets <mistermark009@gmail.com>
2022-06-10 21:43:23 +0300

    Add initials logo component

---
+++ src/components/InitialsLogo.tsx
@@ -0,0 +1,20 @@
+const InitialsLogo = () => {
+    return (
+        <svg
+            id="logo"
+            width="50"
+            height="50"
+            viewBox="0 0 100 100"
+        >
+            <path
+                d="M 10 80 L 10 20 L 34 58 L 58 14 L 58 80 L 58 50 L 90 50 L 90 14 L 90 80"
+                stroke="#eee"
+                stroke-width="5"
+                fill="transparent"
+            />
+        </svg>
+    )
+}
+
+
+export default InitialsLogo
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,14 @@
 import { Link } from "react-router-dom"
 import "../styles/Navbar.scss"
 import "../styles/general.scss"
+import InitialsLogo from "../components/InitialsLogo"
 
 
 const Navbar = () => {
     return (
         <nav id="nav">
             <Link to="/" id="logo-link">
-                <svg
-                    id="logo"
-                    width="50"
-                    height="50"
-                    viewBox="0 0 100 100"
-                >
-                    <path
-                        d="M 10 80 L 10 20 L 34 58 L 58 14 L 58 80 L 58 50 L 90 50 L 90 14 L 90 80"
-                        stroke="#eee"
-                        stroke-width="5"
-                        fill="transparent"
-                    />
-                </svg>
+                <InitialsLogo />
             </Link>
 
             <div className="links">
