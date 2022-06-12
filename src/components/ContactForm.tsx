12a06da7721717dbdbba7353b291bf797e4f32c8
hatemets <mistermark009@gmail.com>
2022-06-13 01:52:38 +0300

    Establish initial working version of emailjs

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,56 +1,55 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import emailjs from "@emailjs/browser"
 
 const FORM_ENDPOINT = ""
+const serviceID = process.env.REACT_APP_SERVICE_ID
+const templateID = process.env.REACT_APP_TEMPLATE_ID
+const publicKey = process.env.REACT_APP_PUBLIC_KEY
 
+// NB! .env must exist with the mentioned values
 const ContactForm = () => {
     const [submitted, setSubmitted] = useState(false)
+    const form = useRef();
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setSubmitted(true)
-        console.log("sent")
+    const sendEmail = async (event: any) => {
+        await event.preventDefault()
+        // emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+        // .then(result => console.log(result.text), error => console.log(error.text))
+
+        try {
+            const res = await emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+            setSubmitted(true)
+            console.log("Mail sent successfully!")
+            console.log(res)
+        }
+        catch (err) {
+            console.error("There was an error with sending the form.")
+            console.error(err)
+        }
+
     }
 
-    useEffect(() => {
-        console.log(process.env.REACT_APP_PUBLIC_KEY)
-        console.log(process.env)
-    }, [])
+    // useEffect(() => {
+    //     console.log(process.env.REACT_APP_PUBLIC_KEY)
+    //     console.log(process.env)
+    // }, [])
 
     return (
-        <form
-            action={FORM_ENDPOINT}
-            onSubmit={handleSubmit}
-            method="POST"
-            target="_blank"
-        >
+        <form ref={form} onSubmit={sendEmail}>
+            <input type="hidden" name="contact_number" />
             <div>
-                <h1>{ process.env.REACT_APP_PUBLIC_KEY}</h1>
-                <input
-                    type="text"
-                    placeholder="Your name"
-                    name="name"
-                    required
-                />
+                <label>Name</label>
+                <input type="text" name="user_name" />
             </div>
             <div>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    required
-                />
+                <label>Email</label>
+                <input type="email" name="user_email" />
             </div>
             <div>
-                <textarea
-                    placeholder="Your message"
-                    name="message"
-                    required
-                />
+                <label>Message</label>
+                <textarea name="message"></textarea>
             </div>
-            <div>
-                <input type="submit" value="Send message" />
-            </div>
+            <input type="submit" value="Send" />
         </form>
     )
 }
