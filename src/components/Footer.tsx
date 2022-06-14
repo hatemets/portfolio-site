e713f726fd95804163996a95fb6973ec11880aac
hatemets <mistermark009@gmail.com>
2022-06-14 16:45:20 +0300

    Footer design made responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,19 @@ const Footer = () => {
 
     return (
         <footer id="footer">
-            <a href="mailto:heidmetsmark@gmail.com">
-                <FontAwesomeIcon className="icon email-icon" size={size} icon={faEnvelope} />
-            </a>
+            <div className="container">
+                <a href="mailto:heidmetsmark@gmail.com">
+                    <FontAwesomeIcon className="icon email-icon" size={size} icon={faEnvelope} />
+                </a>
 
-            <a href="https://github.com/hatemets">
-                <FontAwesomeIcon className="icon github-icon" size={size} icon={faGithub} />
-            </a>
+                <a href="https://github.com/hatemets">
+                    <FontAwesomeIcon className="icon github-icon" size={size} icon={faGithub} />
+                </a>
 
-            <a className="linkedin-link" href="https://www.linkedin.com/in/mark-heidmets-969006195/">
-                <FontAwesomeIcon className="icon linkedin-icon" size={size} icon={faLinkedin} />
-            </a>
+                <a className="linkedin-link" href="https://www.linkedin.com/in/mark-heidmets-969006195/">
+                    <FontAwesomeIcon className="icon linkedin-icon" size={size} icon={faLinkedin} />
+                </a>
+            </div>
         </footer>
     )
 }
