4eac27944af3a739d09e0fb2801ab68b4256e947
hatemets <mistermark009@gmail.com>
2022-06-15 14:40:51 +0300

    Add custom larger icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom"
 
 const SideBar = ({ isOpen, setOpen }) => {
-    const display = isOpen ? "block" : "none"
-
     return (
         <>
             { isOpen && 
